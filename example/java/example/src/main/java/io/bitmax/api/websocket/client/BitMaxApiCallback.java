ce0ede74a3f5e0a55787a57e4f7cd8d86dd2b37d
mihail.petrov <mihail.petrov@gn.org>
2019-06-19 00:29:32 +0300

    add 'BitMaxApiCallback' docs

--- example/java/example/src/main/java/io/bitmax/api/websocket/client/BitMaxApiCallback.java
+++ example/java/example/src/main/java/io/bitmax/api/websocket/client/BitMaxApiCallback.java
@@ -1,12 +1,24 @@
 package io.bitmax.api.websocket.client;
 
 /**
+ * BitMaxApiCallback is a functional interface used for webSocket client.
+ *
  * @param <T> the return type from the callback
  */
 @FunctionalInterface
 public interface BitMaxApiCallback<T> {
 
+    /**
+     * Called whenever a response comes back from the BitMax API.
+     *
+     * @param response the expected response object
+     */
     void onResponse(T response);
 
+    /**
+     * Called whenever an error occurs.
+     *
+     * @param cause the cause of the failure
+     */
     default void onFailure(Throwable cause) {}
 }
